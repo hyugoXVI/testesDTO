f4be740752e40b4fbc8af2a22284950ddd8d2790
Hugo Aragao <hugoarly01@gmail.com>
2025-08-31 11:42:06 -0300

    initial commit

---
+++ src/main/java/usuarios/testeConhecimentos/TesteConhecimentosApplication.java
@@ -0,0 +1,13 @@
+package usuarios.testeConhecimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteConhecimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteConhecimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/usuarios/testeConhecimentos/TesteConhecimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package usuarios.testeConhecimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteConhecimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
